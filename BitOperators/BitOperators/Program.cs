c9feff322765c291ef9777d5f94ce37f37ed3fa6
Ionică Bizău <bizauionica@yahoo.com>
2014-11-06 19:02:59 +0200

    Added BitOperators generated project files

---
+++ BitOperators/BitOperators/Program.cs
@@ -0,0 +1,12 @@
+using System;
+
+namespace BitOperators
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
